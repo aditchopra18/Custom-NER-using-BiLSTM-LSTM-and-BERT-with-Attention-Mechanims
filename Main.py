da20c6300337c877a21a0b62c692d6bb1189c20a
Adit Chopra <98331414+aditchopra18@users.noreply.github.com>
2024-05-27 12:38:08 +0530

    modified:   Main.py 	modified:   Tagged_File.txt

--- Main.py
+++ Main.py
@@ -1,9 +1,11 @@
+# Importing the relevant libraries
 import os
 import re
 import string
 
-train_file = 'parsing.txt'
+train_file = 'NCBItrainset_corpus.txt'
 
+# Reading the dataset file
 def read_dataset(file_path):
     with open(file_path, "r") as file:
         lines = file.readlines()
@@ -44,12 +46,13 @@ def parse_paragraph(paragraph):
         sentences.append(sentence)
     return sentences, annotations
 
+# Data Labelling
 def tag_annotations(sentences, annotations):
     tagged_sentences = []
     char_count = 0
 
     for sentence in sentences:
-        tags = ['O'] * len(sentence)
+        tags = ['O'] * len(sentence) # Initially all tags are set at "O"
         word_starts = []
         word_ends = []
         char_pos = 0
@@ -60,6 +63,8 @@ def tag_annotations(sentences, annotations):
             word_ends.append(char_pos)
             char_pos += 1  # WhiteSpace Character
 
+        # Based on the character limits, change the annotations
+        # A custom IO tagging scheme is used
         for start, end, disease_info, label in annotations:
             for i, (word_start, word_end) in enumerate(zip(word_starts, word_ends)):
                 if word_start >= start and word_end <= end:
@@ -71,6 +76,7 @@ def tag_annotations(sentences, annotations):
 
     return tagged_sentences
 
+# Parsing the dataset file
 lines = read_dataset(train_file)
 paragraphs = parse_dataset(lines)
 
@@ -81,9 +87,12 @@ for paragraph in paragraphs:
     tagged_sentences = tag_annotations(sentences, annotations)
     all_tagged_sentences.extend(tagged_sentences)
 
+# Output File containing all the tags
 output_file = 'Tagged_File.txt'
 with open(output_file, 'w') as file:
     for sentence, tags in all_tagged_sentences:
         for word, tag in zip(sentence, tags):
             file.write(f'{word}\t{tag}\n')
         file.write('\n')
+
+# Training the Data Set
